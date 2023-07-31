c467cdcc5e177ae0fd175ddcab883346fdb7316b
Lucas Chan <chankaihong1992118@gmail.com>
2023-07-31 15:13:58 +0800

    Edited prodConfig

--- src/main/java/com/fsse2305/e_commerce_project/config/EnvConfig.java
+++ src/main/java/com/fsse2305/e_commerce_project/config/EnvConfig.java
@@ -2,5 +2,5 @@ package com.fsse2305.e_commerce_project.config;
 
 public class EnvConfig {
     public static final String devConfig = "http://localhost:5173/";
-    public static final String prodConfig = "https://api.monsteruniverse.online";
+    public static final String prodConfig = "https://store.monsteruniverse.online";
 }
